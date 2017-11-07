06835d8fe1210222a95efd5e87c46cc9f828c022
chrynuik <catherine.hrynuik@gmail.com>
2017-11-07 01:10:09 +0000

    console loggin avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,30 @@
 var request = require('request');
+var secrets = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': `token ${secrets.GITHUB_TOKEN}`
+    }
+
+  };
+
+  request(options, (err, res, body) => {
+    cb(err, body);
+  });
 }
+
+getRepoContributors("jquery", "jquery", (err, results) => {
+  console.log("Errors:", err);
+  console.log("Result:", results);
+  const contributors = JSON.parse(results);
+  contributors.forEach((contributor) => {
+    console.log(contributor.avatar_url);
+  });
+
+
+});
